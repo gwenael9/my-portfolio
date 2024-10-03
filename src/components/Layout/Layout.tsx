ef904d6bb06d3b61b8a2b6d65d097bede8006e0b
gwenael9 <gwenaelgueho@gmail.com>
2024-10-03 21:47:52 +0200

    balise meta

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -19,7 +19,46 @@ export default function Layout({ children, title }: LayoutProps) {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{`Gwenaël Guého - ${title}`}</title>
+        <meta name="title" content="Gwenaël Guého - Portfolio" />
+        <meta
+          name="description"
+          content="Bienvenue sur mon portfolio. Découvrez mes projets en tant que développeur fullstack, alliant expertise frontend et backend pour créer des solutions web performantes et élégantes."
+        />
+        <meta
+          name="keywords"
+          content="développeur fullstack, portfolio, projets, frontend, backend, développement web"
+        />
+        <meta name="author" content="Gwenaël Guého" />
+        <meta property="og:title" content="Gwenaël Guého - Portfolio" />
+        <meta
+          property="og:description"
+          content="Découvrez mon portfolio et mes projets en tant que développeur fullstack."
+        />
+        <meta property="og:image" content="/portfolio.png" />
+        <meta property="og:url" content="https://www.gwenaelgueho.com/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Gwenaël Guého - Développeur Fullstack"
+        />
+        <meta
+          name="twitter:description"
+          content="Découvrez mon portfolio et mes projets en tant que développeur fullstack."
+        />
+        <meta name="twitter:image" content="/portfolio.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="og:image:alt"
+          content="Aperçu du portfolio de Gwenaël Guého"
+        />
+        <meta
+          name="twitter:image:alt"
+          content="Aperçu du portfolio de Gwenaël Guého"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
